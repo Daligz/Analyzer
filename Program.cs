d4f585d47a2014679461de9a4b3f3cc46625c6df
Dali <oscardali370@gmail.com>
2022-01-23 19:34:22 -0600

    Mini menu implemented

--- Program.cs
+++ Program.cs
@@ -8,13 +8,19 @@ namespace Analyzer
     {
         static void Main(string[] args)
         {
-            Console.Write("Escribe alguna expresión: ");
-            string expression = Console.ReadLine();
-            ExpressionsRules.RunChecks(ref expression);
-            Parser parser = new Parser(expression);
-            parser.Compute();
-            parser.Print();
-            parser.Check();
+            do
+            {
+                Console.Clear();
+                Console.Write("Escribe una expresión: ");
+                string expression = Console.ReadLine();
+                ExpressionsRules.RunChecks(ref expression);
+                Parser parser = new Parser(expression);
+                parser.Compute();
+                parser.Print();
+                parser.Check();
+                Console.WriteLine("\n\n(Escribe \"q\" para salir)");
+                Console.WriteLine("(Presiona cualquier tecla para escribir otra expresión)");
+            } while (Console.ReadKey().Key != ConsoleKey.Q);
         }
     }
 }
